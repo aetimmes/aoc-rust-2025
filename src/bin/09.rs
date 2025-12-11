39eaeef6893b494c235fad372c517154c71cc94c
Andrew E. Timmes <andrew.timmes@gmail.com>
2025-12-10 20:17:56 -0500

    wip

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,17 +1,40 @@
 advent_of_code::solution!(9);
+use std::cmp::max;
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let points: Vec<(u64, u64)> = Vec::new();
+    let mut points: Vec<(u64, u64)> = Vec::new();
+    let mut result: u64 = 0;
     for line in input.lines() {
-        (c1, c2) = line
-            .split_once(",")?
-            .map(|x| (u64::from_str_radix(x.0, 10), u64::from_str_radix(x.0, 10)));
+        let t = line.split_once(",")?;
+        let Ok(c1) = u64::from_str_radix(t.0, 10) else {todo!()};
+        let Ok(c2) = u64::from_str_radix(t.1, 10) else {todo!()};
+
+        let points_iter= points.iter();
+        for p in points_iter {
+            result = max(result, (p.0.abs_diff(c1) + 1) * (p.1.abs_diff(c2) + 1) );
+        }
+
+        points.push((c1,c2));
     }
-    None
+    Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut points: Vec<(u64, u64)> = Vec::new();
+    let mut result: u64 = 0;
+    for line in input.lines() {
+        let t = line.split_once(",")?;
+        let Ok(c1) = u64::from_str_radix(t.0, 10) else {todo!()};
+        let Ok(c2) = u64::from_str_radix(t.1, 10) else {todo!()};
+
+        let points_iter= points.iter();
+        for p in points_iter {
+            result = max(result, (p.0.abs_diff(c1) + 1) * (p.1.abs_diff(c2) + 1) );
+        }
+
+        points.push((c1,c2));
+    }
+    Some(result)
 }
 
 #[cfg(test)]
@@ -21,7 +44,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(50));
     }
 
     #[test]
